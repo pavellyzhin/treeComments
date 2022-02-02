6fb7b9eefbfccb74445e9c25381a2b7571f4682e
pavelLyzhin <pavellyzhin@yandex.ru>
2022-02-02 15:07:43 +0300

    m

---
+++ sql/comment.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win32 (AMD64)
+--
+-- Host: localhost    Database: comment
+-- ------------------------------------------------------
+-- Server version	5.5.62
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` smallint(1) unsigned NOT NULL AUTO_INCREMENT,
+  `parentCommentId` smallint(1) unsigned DEFAULT NULL,
+  `publicationId` smallint(1) unsigned NOT NULL,
+  `comment` tinytext,
+  `userId` smallint(1) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,NULL,1,NULL,NULL),(2,NULL,1,NULL,NULL),(3,NULL,1,NULL,NULL),(4,NULL,1,NULL,NULL),(5,NULL,1,NULL,NULL),(6,NULL,1,NULL,NULL),(7,1,1,NULL,NULL),(8,2,1,NULL,NULL),(9,1,1,NULL,NULL),(10,2,1,NULL,NULL),(11,2,1,NULL,NULL),(12,2,1,NULL,NULL),(13,2,1,NULL,NULL),(14,3,1,NULL,NULL),(15,6,1,NULL,NULL),(16,8,1,NULL,NULL);
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-02 15:00:54
